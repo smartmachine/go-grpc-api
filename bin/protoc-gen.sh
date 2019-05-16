65dc0aa813f60560b5693f3e3d71db5f8f46b481
Conrad Christensen <conrad.christensen@gmail.com>
2019-05-16 23:18:06 +0200

    Added REST gateway to gRPC

--- bin/protoc-gen.sh
+++ bin/protoc-gen.sh
@@ -2,7 +2,7 @@
 go build -o ./bin/generators/protoc-gen-go           ./vendor/github.com/golang/protobuf/protoc-gen-go
 go build -o ./bin/generators/protoc-gen-grpc-gateway ./vendor/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 go build -o ./bin/generators/protoc-gen-swagger      ./vendor/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
-protoc --plugin=bin/generators/protoc-gen-go           --proto_path=api/proto/v1 --go_out=plugins=grpc:pkg/api/v1               todo-service.proto
-protoc --plugin=bin/generators/protoc-gen-grpc-gateway --proto_path=api/proto/v1 --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo-service.proto
-protoc --plugin=bin/generators/protoc-gen-swagger      --proto_path=api/proto/v1 --swagger_out=logtostderr=true:api/swagger/v1  todo-service.proto
+protoc --plugin=bin/generators/protoc-gen-go           --proto_path=third_party --proto_path=api/proto/v1 --go_out=plugins=grpc:pkg/api/v1               todo-service.proto
+protoc --plugin=bin/generators/protoc-gen-grpc-gateway --proto_path=third_party --proto_path=api/proto/v1 --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo-service.proto
+protoc --plugin=bin/generators/protoc-gen-swagger      --proto_path=third_party --proto_path=api/proto/v1 --swagger_out=logtostderr=true:api/swagger/v1  todo-service.proto
 
